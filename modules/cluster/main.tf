61cb34c184d8b223212ff54fc7d68a67ab8cfcd9
Alex Brodov <alex.suw@gmail.com>
2022-06-26 04:10:26 +0300

    VN-2605 - more syntax fixes for tf12

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -158,7 +158,7 @@ resource "aws_autoscaling_group" "asg" {
   name = "${var.name}-asg"
 
   launch_configuration = "${aws_launch_configuration.instance.name}"
-  vpc_zone_identifier  = ["${var.vpc_subnets}"]
+  vpc_zone_identifier  = var.vpc_subnets
   max_size             = "${var.asg_max_size}"
   min_size             = "${var.asg_min_size}"
   desired_capacity     = "${var.asg_desired_size}"
